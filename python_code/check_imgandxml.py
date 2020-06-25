9252f8c878f79ca7e8fb9fbe0b7daeb3fd9ca542
fukudashuichi <fukudaubuntu@gmail.com>
2020-06-25 15:22:51 +0900

    filenameの変更コードの追加, <path>の変更

--- python_code/check_imgandxml.py
+++ python_code/check_imgandxml.py
@@ -7,26 +7,28 @@ from PIL import Image
 
 if __name__ == '__main__':
     xmlfolderpath = "./../FishDB_annotations_png_xml/"
-    xmlfolderpath = "./../valid_xml_jpg/"
+    #xmlfolderpath = "./../valid_xml_jpg/"
 
     #xmlfolderpath = "./../koura/xml/"
     #xmlfolderpath = "./../UnderwaterPhotography/xml/"
     imgfilepath = "./../FishDB_images_png/"
-    imgfilepath = "./../valid_img_jpg/"
+    #imgfilepath = "./../valid_img_jpg/"
     #imgfilepath = "./../koura/png/"
     #imgfilepath = "./../UnderwaterPhotography/nagisa_park/"
-    outputxmlpath=xmlfolderpath+'output/'
+    #outputxmlpath=xmlfolderpath+'output/'
+    outputxmlpath=xmlfolderpath
     os.makedirs(outputxmlpath, exist_ok=True)
 
     xmlfiles = [ x.split(".")[0] for x in sorted(os.listdir(xmlfolderpath)) if x!=".DS_Store" and x!="output"]
     imgfiles = [ x.split(".")[0] for x in sorted(os.listdir(imgfilepath)) if x!=".DS_Store"]
+    imgfiles_exts=[ x.split(".")[-1] for x in sorted(os.listdir(imgfilepath)) if x!=".DS_Store"]
 
     print(xmlfiles)
     #print(imgfiles)
     print("##################### check xml files")
-    for xmlfilename in xmlfiles:
+    for i,xmlfilename in enumerate(xmlfiles):
         if not xmlfilename in imgfiles:
-            print( "Error: "+xmlfilename+".jpg" +" are not found. Please search it, or delete "+xmlfilename)
+            print( "Error: "+xmlfilename+ "."+imgfiles_exts[i] +" are not found. Please search it, or delete "+xmlfilename)
         #######
         with open(xmlfolderpath+xmlfilename+".xml") as f:
             rawfile = f.read()
@@ -38,9 +40,12 @@ if __name__ == '__main__':
         targetfilename=re.findall("<filename>.*</filename>",rawfile)[0]
         print(targetfilename)
         #print(rawfile)
-        rawfile2=re.sub(r".png</filename>",r".jpg</filename>",rawfile)
-        #print(rawfile2)
-        im = Image.open(imgfilepath+xmlfilename+".jpg")
+        # filename の変更
+        #rawfile=re.sub(r".png</filename>",r".jpg</filename>",rawfile)
+        #print(rawfile)
+        # <path>の削除
+        rawfile=re.sub(r"<path>.*</path>","",rawfile)
+        im = Image.open(imgfilepath+xmlfilename+"."+imgfiles_exts[i])
         #print(im.size)
         img_width=int(im.size[0])
         img_height=int(im.size[1])
@@ -50,15 +55,15 @@ if __name__ == '__main__':
         if height!=img_height:
             print("different height ({0},{1}): {2}".format(height,img_height,imgfilepath+xmlfilename) )
 
-        #
+        # ファイルの書き込み
         with open(outputxmlpath+xmlfilename+".xml",mode='w') as f:
-            f.write(rawfile2)
+            f.write(rawfile)
 
 
     print("##################### check img files")
     for i,imgfilename in enumerate(imgfiles):
         if not imgfilename in xmlfiles:
-            print("Error: No annotation file. " + imgfilename + ".jpg" + " will be deleted, [y/n] ")
+            print("Error: No annotation file. " + imgfilename + "."+imgfiles_exts[i]+  " will be deleted, [y/n] ")
             ans=input()
             if ans=="y":
                 print(imgfilepath+imgfilename+"."+sorted(os.listdir(imgfilepath))[i].split(".")[1]+" is deleted.")
